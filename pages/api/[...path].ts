030d2bafc39a9431afcf80547e8ad0d4a8c88d15
NgTheLuan <nguyenluan.work@gmail.com>
2024-09-17 11:15:40 +0700

    Proxy requests to API Server

---
+++ pages/api/[...path].ts
@@ -0,0 +1,36 @@
+import Cookies from 'cookies'
+import httpProxy from 'http-proxy'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+}
+
+const proxy = httpProxy.createProxyServer()
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+	return new Promise((resolve) => {
+		// convert cookies to header Authorization
+		const cookies = new Cookies(req, res)
+		const accessToken = cookies.get('access_token')
+		if (accessToken) {
+			req.headers.Authorization = `Bearer ${accessToken}`
+		}
+
+		// don't send cookies to API server
+		req.headers.cookie = ''
+
+		// API_URL = https://js-post-api.herokuapp.com
+		proxy.web(req, res, {
+			target: process.env.API_URL,
+			changeOrigin: true,
+			selfHandleResponse: false,
+		})
+
+		proxy.once('proxyRes', () => {
+			resolve(true)
+		})
+	})
+}
