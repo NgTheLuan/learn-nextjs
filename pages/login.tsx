9d6d25ae32539162ba2157ee587d3277f1ace00a
NgTheLuan <nguyenluan.work@gmail.com>
2024-09-18 10:35:47 +0700

    Integrate API for Login Page

---
+++ pages/login.tsx
@@ -0,0 +1,52 @@
+// import { useAuth } from '@/hooks/use-auth'
+import authApi from 'api-client/auth-api'
+import { useRouter } from 'next/router'
+
+export default function LoginPage() {
+	const router = useRouter()
+	// const { mutate, login } = useAuth({ revalidateOnMount: false })
+
+	async function handleGetProfileClick() {
+		try {
+			try {
+				await authApi.getProfile()
+			} catch (error) {
+				console.log('failed to get profile', error)
+			}
+		} catch (error) {}
+	}
+
+	async function handleLogoutClick() {
+		try {
+			// await authApi.logout()
+		} catch (error) {
+			console.log('failed to logout', error)
+		}
+	}
+
+	async function handleLoginClick() {
+		try {
+			await authApi.login({
+				username: 'test1',
+				password: '123123',
+			})
+			router.push('/about')
+		} catch (error) {
+			console.log('failed to login', error)
+		}
+	}
+
+	function goToAbout() {
+		router.push('/about')
+	}
+
+	return (
+		<div>
+			<h1>Login Page</h1>
+			<button onClick={handleLoginClick}>Login</button>
+			<button onClick={handleGetProfileClick}>Get profile</button>
+			<button onClick={goToAbout}>Go to About</button>
+			<button onClick={handleLogoutClick}>Logout</button>
+		</div>
+	)
+}
