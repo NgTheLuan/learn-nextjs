41b5e04a4f026c2c967d1473272b6f6fdc0065ce
NgTheLuan <nguyenluan.work@gmail.com>
2024-09-18 15:09:26 +0700

    ..

--- components/swr/student-detail.tsx
+++ components/swr/student-detail.tsx
@@ -8,11 +8,7 @@ function StudentDetail({ studentid }: IStudentDetail) {
 	const { data, error, mutate, isValidating } = useSWR(`/students/${studentid}`, {
 		revalidateOnFocus: false,
 	})
-	return (
-		<div>
-			{data?.name || '--'} - {data?.gender || ''} - {data?.age || '--'}
-		</div>
-	)
+	return <div>{data ? `${data?.name} - ${data?.gender} - ${data?.age}` : '-loading-'}</div>
 }
 
 export default StudentDetail
